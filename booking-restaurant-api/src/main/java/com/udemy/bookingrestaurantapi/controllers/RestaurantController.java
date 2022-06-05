7c4d45167d724786b0f68e816029863322d064b3
U0192442 <crojas@opentrends.net>
2022-06-05 12:14:49 +0200

    Se han hecho cambios en las versiones de algunas dependencias de pom.xml

--- booking-restaurant-api/src/main/java/com/udemy/bookingrestaurantapi/controllers/RestaurantController.java
+++ booking-restaurant-api/src/main/java/com/udemy/bookingrestaurantapi/controllers/RestaurantController.java
@@ -2,7 +2,10 @@ package com.udemy.bookingrestaurantapi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.bookingrestaurantapi.exceptions.BookingException;
@@ -17,6 +20,9 @@ public class RestaurantController {
 	@Autowired
 	RestaurantService restaurantService;
 
+@ResponseStatus(HttpStatus.OK)
+@RequestMapping(value = "restaurant" + "/{" + "restaurantId"+ "}", 
+		method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public BookingResponse<RestaurantRest> getRestaurantById(Long restaurantId) throws BookingException {
 
 		return new BookingResponse<>("Succes", String.valueOf(HttpStatus.OK), "OK",
